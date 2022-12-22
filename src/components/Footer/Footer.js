3bee0a935c5541089e4b8e1e5188e7d6ef2e0fb6
Faytey7 <faithroberts708@gmail.com>
2022-12-22 14:46:00 +0100

    adding some icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,24 @@
 import "./Footer.css";
+import linkedIn from "../../images/linkedin-logo.svg";
+import twitter from "../../images/twitter.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="footer">
       <p>Copyrights &copy; Faith Mama Roberts 2022. All Rights Reserved.</p>
+      <Link
+        to={{ pathname: "https://www.linkedin.com/in/faytey7" }}
+        target="_blank"
+      >
+        <img src={linkedIn} alt="LinkedIn Profile" />
+      </Link>
+      <Link
+        to={{ pathname: "https://www.twitter.com/faytey7" }}
+        target="_blank"
+      >
+        <img src={twitter} alt="Twitter Profile" />
+      </Link>
     </div>
   );
 };
