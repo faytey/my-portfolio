108c813655155a8327b2665f2146c8da45740398
Faytey7 <faithroberts708@gmail.com>
2022-12-26 16:09:35 +0100

    adding navbar responsivity and functionalities

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,46 +1,51 @@
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
-  // const [toggler, setToggler] = useState(true);
+  const [toggler, setToggler] = useState(false);
+  const [screen, setScreen] = useState(window.innerWidth);
+  const toggleMenu = () => {
+    setToggler(!toggler);
+  };
 
-  // const handleClick = () => {
-  //   setToggler(!toggler)
-  // }
+  useEffect(() => {
+    const width = () => {
+      setScreen(window.innerWidth);
+    };
 
-  // if(toggler){
-  //   return
-
-  // }
+    return () => {
+      window.removeEventListener("resize", width);
+    };
+  }, []);
   return (
     <nav className="nav">
-      <Link to="/">
-        <div className="nav-brand">FAITH R.</div>
-      </Link>
-      <div className="toggle">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      <div className="collapse">
-        <span></span>
-        <span></span>
-      </div>
-      <ul className="nav-list">
+      <div className="navs">
         <Link to="/">
-          <li className="nav-item">HOME</li>
+          <div className="nav-brand">FAITH R.</div>
         </Link>
-        <Link to="/about">
-          <li className="nav-item">ABOUT</li>
-        </Link>
-        <Link to="/skills">
-          <li className="nav-item">SKILLS</li>
-        </Link>
-        <Link to="/contact">
-          <li className="nav-item">CONTACT</li>
-        </Link>
-      </ul>
+        <div className="toggle" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
+      {(toggler || screen > 900) && (
+        <ul className="nav-list">
+          <Link to="/">
+            <li className="nav-item">HOME</li>
+          </Link>
+          <Link to="/about">
+            <li className="nav-item">ABOUT</li>
+          </Link>
+          <Link to="/skills">
+            <li className="nav-item">SKILLS</li>
+          </Link>
+          <Link to="/contact">
+            <li className="nav-item">CONTACT</li>
+          </Link>
+        </ul>
+      )}
     </nav>
   );
 };
